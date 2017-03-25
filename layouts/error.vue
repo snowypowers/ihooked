b23e0537eec49a12424cde15e35839254d2e915f
Yak Jun Xiang <snowypowers@gmail.com>
2017-03-26 02:08:29 +0800

    use stylus and resize error splash

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 section.container
-  <img src="../assets/img/logo.png" alt="Nuxt.js Logo" class="logo" />
+  img.logo(src="../assets/img/logo.png" alt="Nuxt.js Logo")
   h1.title {{ error.statusCode }}
   h2.info {{ error.message }}
   nuxt-link.button(to="/" v-if="error.statusCode === 404") Homepage
@@ -11,20 +11,23 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="stylus" scoped>
 .title
-{
   margin-top: 15px;
   font-size: 5em;
-}
+
+.logo
+  width: 100%
+  max-width: 300px
+  height: auto
+  margin-bottom: 50px
+
 .info
-{
   font-weight: 300;
   color: #9aabb1;
   margin: 0;
-}
+
 .button
-{
   margin-top: 50px;
-}
+
 </style>
